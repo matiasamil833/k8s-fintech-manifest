6f665f623cfd067cb511b5c33dcd515c0ed73956
matiasamil833 <matias.amil833@comunidadunir.net>
2026-06-19 19:02:20 +0200

    fix: add sleep commands for synchronization and update frontend ConfigMap to use EXTERNAL_IP

--- deploy-app.sh
+++ deploy-app.sh
@@ -6,6 +6,7 @@ kubectl apply -f database/pvc.yaml
 kubectl apply -f database/configmap.yaml
 kubectl apply -f database/service.yaml
 kubectl apply -f database/statefulset.yaml
+sleep 10
 ### App
 kubectl apply -f namespace-app.yaml
 kubectl apply -f secrets/app-db-secret.yaml
@@ -14,8 +15,13 @@ kubectl apply -f backend/configmap.yaml
 kubectl apply -f backend/service.yaml
 kubectl apply -f backend/deployment.yaml
 kubectl apply -f backend/hpa.yaml
+sleep 10
 ### Frontend
-kubectl apply -f frontend/configmap.yaml
+export EXTERNAL_IP=$(kubectl get svc backend -n fintech-app -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+envsubst < frontend/configmap.yaml > frontend/configmap.tmp.yaml
+kubectl apply -f frontend/configmap.tmp.yaml
+rm frontend/configmap.tmp.yaml
+unset EXTERNAL_IP
 kubectl apply -f frontend/service.yaml
 kubectl apply -f frontend/deployment.yaml
 kubectl apply -f frontend/hpa.yaml
